f4fd87ea8eb871d848e9acc29960fc1f712d7cac
Wei Ye <vejuhust@gmail.com>
2014-12-10 01:02:59 +0000

    add rotate_matrix with basic input/output function

---
+++ array/rotate_matrix/rotate_matrix.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLEN 1024
+
+int matrix [MAXLEN][MAXLEN];
+int len_mat;
+
+void rotate_matrix(int a[MAXLEN][MAXLEN], int len) {
+    a[0][0] = 111;
+}
+
+int data_input(char * filename) {
+    FILE * fp;
+    if (NULL == (fp = fopen(filename, "r"))) {
+        printf("failed to read '%s'.\n", filename);
+        return 1;
+    }
+    else {
+        if (0 != fscanf(fp, "%d", &len_mat)) {
+            for (int i = 0; i < len_mat; i++) {
+                for (int j = 0; j < len_mat; j++) {
+                    fscanf(fp, "%d", &(matrix[i][j]));
+                }
+            }
+            fclose(fp);
+            return 0;
+        }
+    }
+    return 2;
+}
+
+int data_output(char * filename) {
+    FILE * fp;
+    if (NULL == (fp = fopen(filename, "w"))) {
+        printf("failed to write '%s'.\n", filename);
+        return 1;
+    }
+    else {
+        fprintf(fp, "%d\n", len_mat);
+        for (int i = 0; i < len_mat; i++) {
+            for (int j = 0; j < len_mat; j++) {
+                fprintf(fp, "%d ", matrix[i][j]);
+            }
+            fprintf(fp, "\n");
+        }
+        fclose(fp);
+    }
+    return 0;
+}
+
+int main() {
+    if (0 == data_input("input.txt")) {
+        rotate_matrix(matrix, len_mat);
+        if (0 == data_output("output.txt")) {
+            return 0;
+        }
+    }
+    return 1;
+}
