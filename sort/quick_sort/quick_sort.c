1f24a2f14c8622c66395492a480b1b472c7f4ca3
Wei Ye <vejuhust@gmail.com>
2014-12-21 13:18:01 +0800

    improve quick_sort with shuffle preprocess - 0m9.781s/0m2.014s/0m5.213s

--- sort/quick_sort/quick_sort.c
+++ sort/quick_sort/quick_sort.c
@@ -3,12 +3,29 @@
 #include <string.h>
 #include <stdbool.h>
 #include <limits.h>
+#include <time.h>
 
 #define MAXLEN 16384
 #define swap(x,y) { long tmp; tmp = (x); (x) = (y); (y) = tmp; }
 
 long data [MAXLEN];
 int  length = 0;
+
+int randint(int low, int high) {
+   static bool flag = true;
+   if (flag) {
+       srand(time(NULL));
+       flag = false;
+   }
+   return rand() % (high - low + 1) + low;
+}
+
+void shuffle(long * data, int length) {
+    for (int i = 0; i < length; i++) {
+        int j = randint(i, length - 1);
+        swap(data[i], data[j]);
+    }
+}
 
 void quick_sort(long * data, int low, int high) {
     long x = data[(low + high) >> 1];
@@ -71,6 +88,7 @@ int data_output(char * filename) {
 
 int main() {
     if (0 == data_input("input.txt")) {
+        shuffle(data, length);
         quick_sort(data, 0, length - 1);
         if (0 == data_output("output.txt")) {
             return 0;
